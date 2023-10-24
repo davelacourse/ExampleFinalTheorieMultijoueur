0cb0221ab1735c80e69e10d20470bd844faf53a3
LaCoors <dave.lacourse@cegeptr.qc.ca>
2023-10-23 23:51:50 -0400

    fin théorie Lobby

--- Assets/_MesAssets/Scripts/Network/LobbyManager.cs
+++ Assets/_MesAssets/Scripts/Network/LobbyManager.cs
@@ -2,15 +2,35 @@ using UnityEngine;
 using Unity.Services.Lobbies;
 using Unity.Services.Lobbies.Models;
 using System.Collections.Generic;
+using Unity.Services.Authentication;
 
 public class LobbyManager : MonoBehaviour
 {
     public static LobbyManager Instance; // Création du Singleton
 
+    private float _timer = 0f; // Timer pour envoyé le signal au Lobby 
+    private Lobby _currentLobby; // Lobby courant
+    
     // Singleton
     private void Awake()
     {
         Instance = this;
+    }
+
+    private void Update()
+    {
+        if (_timer > 15f) 
+        {
+            _timer -= 15f;
+            
+            // Condition pour que seulement le host envoi le ping et non pas tous les joueurs du lobby
+            if(_currentLobby != null && _currentLobby.HostId == AuthenticationService.Instance.PlayerId)
+            {
+                // Envoi un ping au lobby à toutes les 15 secondes
+                LobbyService.Instance.SendHeartbeatPingAsync(_currentLobby.Id);
+            }
+        }
+        _timer += Time.deltaTime;
     }
 
     // Structure complexe qui contient l'information sur un lobby
@@ -37,28 +57,35 @@ public class LobbyManager : MonoBehaviour
         lobbyOptions.Data.Add("JoinCodeKey", dataObject);
 
         // Je créer le Lobby sur le Relay
-        await Lobbies.Instance.CreateLobbyAsync(lobbyData.lobbyName, lobbyData.maxPlayer,lobbyOptions);
+        _currentLobby = await Lobbies.Instance.CreateLobbyAsync(lobbyData.lobbyName, lobbyData.maxPlayer,lobbyOptions);
     }
 
     // Méthode publique pour la partie rapide qui rejoint le premier Lobby
     public async void PartieRapideLobby()
     {
         // Récupère le premier lobby dans la variable lobby
-        Lobby lobby = await Lobbies.Instance.QuickJoinLobbyAsync();
+        _currentLobby = await Lobbies.Instance.QuickJoinLobbyAsync();
         // Va chercher le Joincode du lobby récupéré
-        string relayJoinCode = lobby.Data["JoinCodeKey"].Value;
+        string relayJoinCode = _currentLobby.Data["JoinCodeKey"].Value;
         Debug.Log(relayJoinCode);
         // Rejoint le lobby avec le joinCode récupéré
-        RelayManager.Instance.JoinRelayGame(relayJoinCode);
+        if (_currentLobby != null)
+        {
+            RelayManager.Instance.JoinRelayGame(relayJoinCode);
+        }
+        else
+        {
+            Debug.Log("AUCUN LOBBY DISPONIBLE !!!!!!");
+        }
     }
 
     // Méthode publique pour rejoindre un Lobby précis
     public async void RejoindreLobby(string lobbyID)
     {
         // Récupère le lobby avec le ID reçu en paramètre
-        Lobby lobby = await Lobbies.Instance.JoinLobbyByIdAsync(lobbyID);
+        _currentLobby = await Lobbies.Instance.JoinLobbyByIdAsync(lobbyID);
         // Va chercher le Joincode du lobby récupéré
-        string relayJoinCode = lobby.Data["JoinCodeKey"].Value;
+        string relayJoinCode = _currentLobby.Data["JoinCodeKey"].Value;
         // Rejoint le lobby avec le joinCode récupéré
         RelayManager.Instance.JoinRelayGame(relayJoinCode);
     }
--- Assets/_MesAssets/Scripts/UI/UIJoindreLobby.cs
+++ Assets/_MesAssets/Scripts/UI/UIJoindreLobby.cs
@@ -4,5 +4,49 @@ using Unity.Services.Lobbies.Models;
 
 public class UIJoindreLobby : MonoBehaviour
 {
-     
+    //Conteneur des boutons
+    [SerializeField] private Transform _contentParent = default;
+    [SerializeField] private LobbyListElement _lobbyListElementPrefab = default;
+    [SerializeField] private float _tempsRafraichissement = 2f;
+
+    private float _timer = 0;  // Variable de temps pour le rafraichissement de la liste
+
+    // Afficher la liste des lobby quand on active le panneau JoindreLobby
+    private void OnEnable()
+    {
+        UpdateLobbyList();
+    }
+
+    private void Update()
+    {
+        // Lance le update à chaque temps de rafraichissement
+        if (_timer > _tempsRafraichissement)
+        {
+            UpdateLobbyList();
+            _timer -= _tempsRafraichissement;
+        }
+        _timer += Time.deltaTime;
+    }
+
+    // Méthode qui met à jour la liste des lobbys disponibles 
+    public async void UpdateLobbyList()
+    {
+        // Interroge le service Lobby pour récupérer tous les lobbys
+        QueryResponse response = await Lobbies.Instance.QueryLobbiesAsync();
+
+        // On commence par effacer tous les boutons avant de remettre les actuels
+        for (int i = 0; i < _contentParent.childCount; i++)
+        {
+            Destroy(_contentParent.GetChild(i).gameObject);
+        }
+
+        // Pour chaque Lobby récupérer dans response
+        foreach (var lobby in response.Results)
+        {
+            // On instancie le prefab à l'intérieur du content et le place dans la variable
+            LobbyListElement spawnElement = Instantiate(_lobbyListElementPrefab, _contentParent);
+            spawnElement.Initialiser(lobby);
+        }
+    }
 }
+
